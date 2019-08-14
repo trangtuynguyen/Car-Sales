250dd416fcea8f02c5c1b56a135bad561c95f76c
Trang Nguyen <ttn2127@columbia.edu>
2019-08-14 17:45:41 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -16,33 +16,7 @@ const App = () => {
       features: []
     },
     store: [
-      { id: 1, name: 'V-6 engine', price: 1500 },
-      { id: 2, name: 'Racing detail package', price: 1500 },
-      { id: 3, name: 'Premium sound system', price: 500 },
-      { id: 4, name: 'Rear spoiler', price: 250 }
-    ]
-  };
-
-  const removeFeature = item => {
-    // dispatch an action here to remove an item
-  };
-
-  const buyItem = item => {
-    // dipsatch an action here to add an item
-  };
-
-  return (
-    <div className="boxes">
-      <div className="box">
-        <Header car={state.car} />
-        <AddedFeatures car={state.car} />
-      </div>
-      <div className="box">
-        <AdditionalFeatures store={state.store} />
-        <Total car={state.car} additionalPrice={state.additionalPrice} />
-      </div>
-    </div>
-  );
+      
 };
 
 export default App;
